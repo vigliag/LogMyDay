f1af9d36c287c2927a0f74a3c812f224fab547ac
Gabriele Viglianisi <vigliag@gmail.com>
2015-09-23 23:54:53 +0200

    Fixed a null pointer exception in FieldManagerActivity

--- app/src/main/java/ovh/vii/logmyday/activities/FieldManagerActivity.java
+++ app/src/main/java/ovh/vii/logmyday/activities/FieldManagerActivity.java
@@ -27,7 +27,7 @@ public class FieldManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_manager);
-
+        db = new Database();
         populateListView();
     }
 
